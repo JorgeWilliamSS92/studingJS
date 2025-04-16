915190db2b3c34773ea4a2cb405159969a0d4ed0
Jorge William <jorge_william92@hotmail.com>
2025-04-16 07:09:36 -0300

    finished project, add css, javascript

--- SetTimeOut/script.js
+++ SetTimeOut/script.js
@@ -1,21 +1,30 @@
 const buttonOne = document.getElementById("one");
 const buttonTwo = document.getElementById("two");
 const p = document.getElementById("p");
+const span = document.getElementById("span");
+
+let interval;
 
 buttonOne.addEventListener("click", () => {
   let i = 1;
-  const interval = setInterval(() => {
+  interval = setInterval(() => {
     if (i <= 15) {
       if (i == 1) {
-        p.innerHTML = `0${i} second`;
+        p.innerHTML = `0${i} <span id="span">second</span>`;
       } else if (i < 10) {
-        p.innerHTML = `0${i} seconds`;
+        p.innerHTML = `0${i} <span id="span">seconds</span>`;
       } else {
-        p.innerHTML = `${i} seconds`;
+        p.innerHTML = `${i} <span id="span">seconds</span>`;
       }
       i++;
     } else {
       clearInterval(interval);
     }
   }, 1000);
+});
+
+buttonTwo.addEventListener("click", () => {
+  clearInterval(interval);
+  p.innerHTML = `0 <span id="span">seconds</span>`;
+  alert("Timer stopped");
 });
