f89a82bfa177b4c9daf9a43c8ecb428b5ed39a01
Jorge William <jorge_william92@hotmail.com>
2025-04-22 07:43:05 -0300

    add a security version to calculation

--- Calculator Program/js/script.js
+++ Calculator Program/js/script.js
@@ -16,7 +16,11 @@ function result() {
       display.textContent = "Error";
       return;
     }
-    display.textContent = eval(display.textContent);
+    let fn = new Function(`return ${display.textContent}`);
+    //eval(display.textContent); this may be dangerous
+    //eval is dangerous because it can execute arbitrary code, which can lead to security vulnerabilities
+    //For example, if a user inputs "alert('Hacked!')", eval will execute that code
+    display.textContent = fn();
     history.textContent = calculationHistory;
   } catch (err) {
     display.textContent = "Error";
